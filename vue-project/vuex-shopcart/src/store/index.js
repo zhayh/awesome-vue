aa41be2d02b2bbc62a09a4f1a6e887c94e4f0ed9
zhayh <zhayh.nj@gmail.com>
2020-04-11 13:56:28 +0800

    vuex的购物车案例

---
+++ vue-project/vuex-shopcart/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import goods from './modules/goods'
+import shopcart from './modules/shopcart'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    goods,
+    shopcart
+  }
+})
